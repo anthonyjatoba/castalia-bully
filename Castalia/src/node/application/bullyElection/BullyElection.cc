c9baef7a01c1feffc158af963029a4512e6ed17c
Anthony Jatobá <anthonyjatoba@gmail.com>
2018-10-03 17:44:49 -0300

    Adds comments

--- Castalia/src/node/application/bullyElection/BullyElection.cc
+++ Castalia/src/node/application/bullyElection/BullyElection.cc
@@ -3,7 +3,7 @@
 Define_Module(BullyElection);
 
 void BullyElection::startup() {
-	sendLeader();
+	sendLeader();									//o líder informa aos demais
 	setTimer(SEND_HEARTBEAT, 5);	//o sink começa a enviar o heartbeat após 5 segundos
 	setTimer(CHECK_LEADER, 10);		//após 10 segundos os nodes começam a checar
 }
@@ -16,12 +16,10 @@ void BullyElection::timerFiredCallback(int index) {
 			break;
 		}
 		case CHECK_LEADER: {
-			if (!isLeader){
-				if (getClock() - lastHeartbeat > 5){		//5 segundos sem receber o heartbeat, detecta a falha
+			if (!isLeader && getClock() - lastHeartbeat > 5) {
 					trace() << self << " detectou a falha do líder " << leaderID;
 					//TODO ELEIÇÃO
 				}
-
 			}
 			setTimer(CHECK_LEADER, 5);
 			break;
@@ -33,31 +31,29 @@ void BullyElection::fromNetworkLayer(ApplicationPacket * genericPacket, const ch
 	BullyElectionDataPacket	*rcvPacket = check_and_cast<BullyElectionDataPacket*>(genericPacket);
 	BullyElectionData theData = rcvPacket->getExtraData();
 
-
 		switch (theData.messageType) {
 			case HEARTBEAT:
 				trace() << self << " received a HB from " << theData.nodeID;
-				lastHeartbeat = getClock();
+				lastHeartbeat = getClock();	//registra o instante do último HB
 				break;
 			case LEADER:
 				trace() << self << " knows the leader " << theData.nodeID;
-				leaderID = theData.nodeID;
+				leaderID = theData.nodeID;	// atualiza a variável de líder
 				break;
 		}
-
-
 }
 
 void BullyElection::sendHeartbeat() {
-	if (isLeader){
-		if (working){
+	if (isLeader) {
+		if (working) {
 			working = rand() % 100 > 70 ? true : false;		//30% de chance de travar
 		} else {
 			working = rand() % 100 > 70 ? true : false;		//Um node travado tem 30% de chance de voltar a funcionar
 		}
 
-		trace() << "O lider" << (working ? " está funcionando normalmente" : " está travado");
+		trace() << "Leader" << (working ? " is working" : " has failed");
 
+		// cria e envia o pacote de HB
 		if (working) {
 			BullyElectionData tmpData;
 			tmpData.nodeID = (unsigned short)self;
@@ -72,7 +68,7 @@ void BullyElection::sendHeartbeat() {
 }
 
 void BullyElection::sendLeader() {
-	if (isLeader){
+	if (isLeader) {
 		BullyElectionData tmpData;
 		tmpData.nodeID = (unsigned short)self;
 		tmpData.messageType = LEADER;
